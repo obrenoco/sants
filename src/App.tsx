da241f77c9698df3037ff45d95e26a9d6605a484
Your Name <bsromeiro@gmail.com>
2020-10-12 16:05:36 -0300

    core | add: landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
+import logoImg from './images/logo.svg';
+
 import './styles/global.css';
+import './pages/landing.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Rio de Janeiro</strong>
+          <span>Rio de Janeiro</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0,0,0, 0.6)" />
+        </a>
+      </div>
     </div>
   );
 }
